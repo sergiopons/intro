7a13242401200c2aaa566479406279134d051c28
sergiopons <72190359+sergiopons@users.noreply.github.com>
2020-10-09 14:16:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let todoItem = "estudiar javascript";
+let index = 0;
+
+if (numero == 0) {
+    console.log(todoItem);
+}
+
+let todoList = [todoItem, "estudiar PHP", "estudiar CSS"];
+
+for (let = index = 0; index <todoList.length; index++) {
+console.log(todoList[index])
+}
+
+console.log(index);
+
+//OBJETOS//
+
+dog = {
+    nombre: "Duna",
+    raza: "pataner",
+    ladra() {
+        return ("GUAU GUAU")
+
+    }
+}
+
+console.log(dog.name);
+console.log(dog.raza);
+dog.bark();
